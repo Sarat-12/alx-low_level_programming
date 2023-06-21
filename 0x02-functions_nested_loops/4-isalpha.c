7a4e0cf169b3f28a5ab7b884fc23d465d4c652d0
Sarat-12 <saratayobam@gmail.com>
2023-06-21 08:14:22 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,6 +10,11 @@ int _isalpha(int c)
 {
   int _isalpha(int c)
   {
-    return((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+    if((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+      {
+	return (1);
+      }
+    return (0);
+      
   }
   
