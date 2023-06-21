b48ef9151b5f1f55cc1485c42f6d42af2bab8085
Sarat-12 <saratayobam@gmail.com>
2023-06-21 07:07:51 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- *print_alphabet_x10 - print the alphabet ten y=times in lower case
- *followed by a new line
+ * print_alphabet_x10 - print the alphabet ten y=times in lower case
+ * followed by a new line
  */
 void print_alphabet_x10(void)
 {
